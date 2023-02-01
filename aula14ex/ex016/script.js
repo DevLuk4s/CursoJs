1e5072c5a39765b7f55a6df1b20ac343d208c552
Lucas Gabriel <lucasgabrielferreirasilva777@gmail.com>
2023-02-01 16:42:09 -0300

    Super Contador

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -3,30 +3,25 @@ function verificar() {
     let fim = document.getElementById('txtfim')
     let pas = document.getElementById('txtpas')
     let res = document.getElementById('res')
-    
     if (ini.value == 0 || fim.value == 0 || pas.value == 0) {
-        res.innerHTML = 'Impossivel contar!'
+        res.innerHTML = 'Impossivel contar'
+        //faltam de dados
     } else {
-        res.innerHTML = 'Contador:'
+        res.innerText = 'Contador:'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(pas.value)
-        if (p <= 0) {
-            window.alert('Passo invalido! Considerando PASSO 1')
-            p = 1
-        }
         if (i < f) {
-            for (let c = i;c <= f;c += p) {
+            for (var c = i;c <= f;c += p) {
                 //contagem crescente
                 res.innerHTML += ` ${c} \u{1F449}`
             }
-            res.innerHTML += `\u{1F3C1}`
         } else {
-            for (let c = i;c >= f;c -= p) {
+            for (var c = i;c >= f;c -= p) {
                 //contagem decrescente
                 res.innerHTML += ` ${c} \u{1F449}`
             }
-            res.innerHTML += `\u{1F3C1}`
         }
+        res.innerHTML += ` \u{1F3C1}`
     }
 }
